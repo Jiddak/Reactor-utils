10f1145f0d449b4aebaff68ca26af32b1356f9d5
Jidder <j_r_kelly@live.co.uk>
2020-06-18 12:09:37 +0100

    Add javadoc for ReactorUtils#joinIf

--- src/main/java/com/jidda/reactorUtils/ReactorUtils.java
+++ src/main/java/com/jidda/reactorUtils/ReactorUtils.java
@@ -45,6 +45,27 @@ public class ReactorUtils {
     }
 
 
+    /**
+     * Map values from two Publishers into time windows and emit combination of values
+     * in case their windows overlap. The emitted elements are obtained by passing the
+     * values from two {@link Publisher}s to a {@link BiPredicate} to determine if emission should occur
+     * and then passing to a {@link BiFunction} to provide resultant value.
+     * <p>
+     * There are no guarantees in what order the items get combined when multiple items from
+     * one or both source Publishers overlap.
+     * @param leftSource the left {@link Publisher} to correlate items with
+     * @param rightSource the right {@link Publisher} to correlate items with
+     * @param resultSelector a {@link BiFunction} that takes items emitted by each {@link Publisher}
+     * and returns the value to be emitted by the resulting {@link Flux}
+     * @param condition {@link BiPredicate} that compares left and right element. If false no emission occurs
+     * The collection is non blocking and can be accessed concurrently
+     * As such, no guarantee can be made that the number of element returned will be exactly cacheSize
+     * @param <TLeft> the type of the elements from the left {@link Publisher}
+     * @param <TRight> the type of the elements from the right {@link Publisher}
+     * @param <R> the combined result type
+     *
+     * @return a joining {@link Flux}
+     */
     public static final <TLeft,TRight,R> Flux<R> joinIf(
             Publisher<TLeft> leftSource,
             Publisher<? extends TRight> rightSource,
@@ -55,6 +76,28 @@ public class ReactorUtils {
     }
 
 
+    /**
+     * Map values from two Publishers into time windows and emit combination of values
+     * in case their windows overlap. The emitted elements are obtained by passing the
+     * values from two {@link Publisher}s to a {@link BiPredicate} to determine if emission should occur
+     * and then passing to a {@link BiFunction} to provide resultant value.
+     * <p>
+     * There are no guarantees in what order the items get combined when multiple items from
+     * one or both source Publishers overlap.
+     * @param leftSource the left {@link Publisher} to correlate items with
+     * @param rightSource the right {@link Publisher} to correlate items with
+     * @param resultSelector a {@link BiFunction} that takes items emitted by each {@link Publisher}
+     * and returns the value to be emitted by the resulting {@link Flux}
+     * @param condition {@link BiPredicate} that compares left and right element. If false no emission occurs
+     * @param cacheSize The number of elements stored for comparison, < 1 is unbounded.
+     * The collection is non blocking and can be accessed concurrently
+     * As such, no guarantee can be made that the number of element returned will be exactly cacheSize
+     * @param <TLeft> the type of the elements from the left {@link Publisher}
+     * @param <TRight> the type of the elements from the right {@link Publisher}
+     * @param <R> the combined result type
+     *
+     * @return a joining {@link Flux}
+     */
     public static final <TLeft,TRight,R> Flux<R> joinIf(
             Publisher<TLeft> leftSource,
             Publisher<? extends TRight> rightSource,
